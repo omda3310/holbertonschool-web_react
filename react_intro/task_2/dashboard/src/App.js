d80c111f89a4375b66fe200ec193dbb74f52b6f0
omda3310 <6197@holbertonstudents.com>
2024-05-06 00:50:34 +0200

    push task_2

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import HolbertonSchool from './HolbertonSchool.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={HolbertonSchool} className="App-logo" alt="logo" />
+        <h1> School dashboard </h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className='for_container'>
+          <label className='lab_email' for='email'>
+            Email:
+          </label>
+          <input type='email' id='email' name='email' /> 
+          <label className='lab_password' for='password'>
+            Password:
+          </label>
+          <input type='password' id='pwd' name='pwd' minLength="8" required />
+          <button className='buttonok'>
+            OK
+          </button>
+        </div>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
