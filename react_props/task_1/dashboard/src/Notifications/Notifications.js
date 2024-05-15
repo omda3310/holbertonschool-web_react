63179fbd02a9d8547bba9c7baa6c930b1e641842
omda3310 <6197@holbertonstudents.com>
2024-05-15 12:29:52 +0200

    All task

---
+++ react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Notifications.';
+import { getLatestNotification } from '../utils/utils';
+
+function Notifications() {
+  return (
+    <div className='Notifications'>
+        <p style={{color: 'black'}}>Here is the list of notifications</p>
+        <ul>
+          <li data-priority='default'>New course available</li>
+          <li dat-priority='urgent'>New resume available</li>
+          <li
+           data-priority='urgent'
+           dangerouslySetInnerHTML={{__html: getLatestNotification()}}>
+          </li>
+        </ul>
+        <button
+          aria-label='close' type='button'
+          style={{
+            position: 'relative',
+            float: 'right',
+            top: '-130px',
+            cursor: 'pointer',
+            background: 'white',
+            border: 'none'
+          }}>x
+        </button>
+
+    </div>
+  )
+}
+
+export default Notifications;
