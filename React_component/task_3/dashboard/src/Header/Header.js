85290788116343a97c5ff51afa8bb285bdd0ea87
omda3310 <6197@holbertonstudents.com>
2024-05-24 00:33:32 +0200

    task_3

---
+++ React_component/task_3/dashboard/src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HolbertonSchool from '../assets/HolbertonSchool.jpg';
+import './Header.css';
+
+
+function Header() {
+  return (
+      <div className="App">
+        <img src={HolbertonSchool} className="App-logo" alt="logo" />
+        <h1> School dashboard </h1>
+      </div>
+  );
+}
+
+export default Header;
