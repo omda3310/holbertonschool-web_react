88c54f9f79bbfca68e6d557386797f3fb7a8c402
omda3310 <6197@holbertonstudents.com>
2024-05-25 00:56:49 +0200

    task_3

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BodySection from './BodySection';
+import { PropTypes } from 'prop-types';
+import './BodySection';
+
+export default class BodySectionWithMarginBottom extends React.Component {
+    render() {
+			return (
+        <div className='bodySectionWithMargin'>
+					<BodySection {...this.props} />
+				</div>
+			);
+    }
+}
+
+BodySectionWithMarginBottom.PropTypes = {
+	title: PropTypes.string,
+	children: PropTypes.node,
+}
