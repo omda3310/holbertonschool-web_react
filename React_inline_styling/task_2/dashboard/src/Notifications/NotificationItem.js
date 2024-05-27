39316b9afb2710f613002ea4accd8dfd0b3cc883
omda3310 <6197@holbertonstudents.com>
2024-05-28 01:27:52 +0200

    task_2

---
+++ React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+const NotificationItem = React.memo(({ id, type = "default", html, value, markAsRead}) => {
+  const handleClick = () => {
+    markAsRead(id);
+  };
+
+  const notifStyle = css(type === "urgent" ? stl.urgent : stl.default);
+
+    if (value) {
+      return (<li className={notifStyle} data-notification-type={type} onClick={handleClick}>{value}</li>);
+    } else {
+      return (
+        <li className={notifStyle} data-notification-type={type} dangerouslySetInnerHTML={html} onClick={markAsRead(id)} />
+      );
+    }
+  })
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string
+  }),
+  markAsRead: PropTypes.func.isRequired,
+  id: PropTypes.number,
+};
+
+const stl = StyleSheet.create({
+  default: {
+    color: "blue",
+  },
+  urgent: {
+    color: "red",
+  },
+});
+
+export default NotificationItem;
---
+++ React_inline_styling/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import NotificationItem from "./NotificationItem";
+import PropTypes from "prop-types";
+import NotificationItemShape from "./NotificationItemShape";
+
+class Notifications extends React.Component {
+	constructor() {
+		super();
+		this.markAsRead = this.markAsRead.bind(this);
+	}
+	markAsRead(id) {
+		console.log(`Notification ${id} has been marked as read`);
+	}
+
+	shouldComponentUpdate(nextProps) {
+		return (
+			nextProps.listNotifications.length > this.props.listNotifications.length
+		);
+	}
+
+	render() {
+		const { displayDrawer, listNotifications } = this.props;
+		return (
+			<>
+				<div className={css(stl.menuItem)}>
+					<p>Your notifications</p>
+				</div>
+				{displayDrawer && (
+					<div className={css(stl.Notifications)}>
+						<button
+							style={{
+								background: "transparent",
+								border: "none",
+								position: "absolute",
+								right: 10,
+							}}
+						>
+							x
+						</button>
+						<p>
+							{ listNotifications.length > 0 ? 'Here is the list of notifications' : 'No new notification for now' }
+						</p>
+						<ul>
+							{listNotifications.length === 0 ? (
+								<NotificationItem value="No new notification for now" />
+							) : (
+								listNotifications.map(notification => (
+									<NotificationItem
+									    key={notification.id}
+										id={notification.id}
+										type={notification.type}
+										value={notification.value}
+										html={notification.html}
+										markAsRead={this.markAsRead}
+									/>
+								)))}
+						</ul>
+					</div>
+				)}
+			</>
+		);
+	};
+}
+
+
+Notifications.defaultProps = {
+	displayDrawer: true,
+	listNotifications: [],
+};
+
+Notifications.propTypes = {
+	displayDrawer: PropTypes.bool,
+	listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+const stl = StyleSheet.create({
+	menuItem: {
+		textAlign: "right",
+		
+	},
+	Notifications: {
+		display: "block",
+		height: "110px",
+		paddingleft: "1rem",
+		border: "dotted",
+		color: "red",
+	}
+});
+
+export default Notifications;
