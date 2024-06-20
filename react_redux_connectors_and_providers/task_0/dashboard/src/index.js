5f855c48522b9eea546477ba9a87efe3223cc875
omda3310 <6197@holbertonstudents.com>
2024-06-20 19:53:26 +0200

    update

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import Provider from 'react-redux';
-import createStore from 'redux';
-import { uiReducer } from "./reducers/uiReducer";
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+import uiReducer from "./reducers/uiReducer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = createStore(uiReducer);
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
